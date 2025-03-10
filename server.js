6d037e1883b8c49fceeb2e271f139578136a6af8
Yevheniia Cherepashchuk <chtozalogin.jh@gmail.com>
2025-03-10 22:54:27 +0100

    server resttart

--- server.js
+++ server.js
@@ -3,6 +3,7 @@ import path from 'path';
 import { fileURLToPath } from 'url';
 import compression from 'compression';
 import helmet from 'helmet';
+import cors from 'cors';
 
 const app = express();
 app.use(
@@ -12,13 +13,20 @@ app.use(
         defaultSrc: ["'self'"],
         scriptSrc: ["'self'"],
         connectSrc: ["'self'", 'https://66b1f8e71ca8ad33d4f5f63e.mockapi.io'],
-        imgSrc: ["'self'", 'data:'],
-        styleSrc: ["'self'", "'unsafe-inline'"],
+        imgSrc: [
+          "'self'",
+          'data:',
+          'https://ftp.goit.study',
+          'https://*.googleusercontent.com',
+        ],
+        styleSrc: ["'self'", "'unsafe-inline'", 'https://fonts.googleapis.com'],
+        fontSrc: ["'self'", 'https://fonts.gstatic.com'],
       },
     },
   }),
 );
 
+app.use(cors()); // Дозволяє запити з будь-якого джерела
 app.use(compression());
 
 const PORT = process.env.PORT || 10000;
