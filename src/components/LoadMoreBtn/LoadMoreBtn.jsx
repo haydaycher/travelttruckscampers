a1ca0dedc9aa87c908f0956455e5a6e442712ffe
Yevheniia Cherepashchuk <chtozalogin.jh@gmail.com>
2025-04-18 01:43:03 +0200

    catalog page adaptive

--- src/components/LoadMoreBtn/LoadMoreBtn.jsx
+++ src/components/LoadMoreBtn/LoadMoreBtn.jsx
@@ -1,19 +1,15 @@
 import css from './LoadMoreBtn.module.css';
 
-const LoadMoreBtn = ({ onClick, disabled }) => {
-  return (
-    <div>
-      <button
-        type="button"
-        onClick={onClick}
-        className={css.buttonLoadMore}
-        disabled={disabled}
-        aria-label="Load more items"
-      >
-        Load more
-      </button>
-    </div>
-  );
-};
+const LoadMoreBtn = ({ onClick, disabled }) => (
+  <button
+    type="button"
+    onClick={onClick}
+    className={css.buttonLoadMore}
+    disabled={disabled}
+    aria-label="Load more items"
+  >
+    Load more
+  </button>
+);
 
 export default LoadMoreBtn;
